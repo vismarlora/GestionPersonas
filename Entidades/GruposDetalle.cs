dba6ebcfa96179b09643d215e9eea48abe4acde9
Enel Almonte <enelramon@hotmail.com>
2021-10-11 09:56:56 -0400

    Agregando entidades iniciales (solo comentario de cuales campos se desean)

---
+++ Entidades/GruposDetalle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPersonas.Entidades
+{
+    public class GruposDetalle
+    {
+        //Id, GrupoId,PersonaId,Orden
+    }
+}
